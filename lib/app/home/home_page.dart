5299a422788b8488c5f42e4ce483c97d420654c9
Sebastian Girsza <sebag96@wp.pl>
2022-08-14 12:47:13 +0200

    Add data from FirebaseFirestore

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -19,32 +20,18 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('My Gym App'),
+      ),
       body: Builder(builder: (context) {
         if (currentIndex == 0) {
-          return Center(
-            child: Text('Jeden'),
-          );
+          return const ProgressPageContent();
         }
         if (currentIndex == 1) {
-          return Center(
-            child: Text('Dwa'),
-          );
+          return const AddExercisesPageContent();
         }
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Jestes zalogowany jako ${widget.user.email}'),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                },
-                child: const Text('Wyloguj'),
-              ),
-            ],
-          ),
-        );
+        return MyAcountPageContent(email: widget.user.email);
       }),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
@@ -69,5 +56,92 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
+  }
+}
+
+class MyAcountPageContent extends StatelessWidget {
+  const MyAcountPageContent({
+    Key? key,
+    required this.email,
+  }) : super(key: key);
+
+  final String? email;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Jestes zalogowany jako $email'),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            child: const Text('Wyloguj'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AddExercisesPageContent extends StatelessWidget {
+  const AddExercisesPageContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Dwa'),
+    );
+  }
+}
+
+class ProgressPageContent extends StatelessWidget {
+  const ProgressPageContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: FirebaseFirestore.instance.collection('gym').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text('Coś poszło nie tak'));
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: Text('Trwa ładowanie'));
+          }
+
+          final documents = snapshot.data!.docs;
+
+          return ListView(
+            children: [
+              for (final document in documents) ...[
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(document['day']),
+                      Text(document['body_part']),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(document['exercises']),
+                          Text(document['number_of_series'].toString()),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ],
+          );
+        });
   }
 }
