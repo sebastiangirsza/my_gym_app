e59ae6f097ee79cf32114917b24d9ad7de1c7d5d
Sebastian Girsza <sebag96@wp.pl>
2022-08-21 15:51:10 +0200

    Move MyApp to app.dart

---
+++ lib/app/app.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:my_gym_app/app/features/home/home_page.dart';
+import 'package:my_gym_app/app/features/login/login_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        canvasColor: Colors.white.withOpacity(0.6),
+      ),
+      home: const RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatelessWidget {
+  const RootPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        final user = snapshot.data;
+        if (user == null) {
+          return LoginPage();
+        }
+        return HomePage(user: user);
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:my_gym_app/app/features/home/home_page.dart';
-import 'package:my_gym_app/app/features/login/login_page.dart';
+import 'package:my_gym_app/app/app.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -11,40 +9,4 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-        canvasColor: Colors.white.withOpacity(0.6),
-      ),
-      home: const RootPage(),
-    );
-  }
-}
-
-class RootPage extends StatelessWidget {
-  const RootPage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        final user = snapshot.data;
-        if (user == null) {
-          return LoginPage();
-        }
-        return HomePage(user: user);
-      },
-    );
-  }
 }
